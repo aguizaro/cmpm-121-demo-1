53f143cdc1521d64c232f0058b83646a892507b3
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-10-09 14:18:26 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,21 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Pizza Party";
-
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header: HTMLHeadElement = document.createElement("h1");
 header.innerHTML = gameName;
 
-const button = document.createElement("button");
+const button: HTMLButtonElement = document.createElement("button");
 button.textContent = "🍕";
 
-app.append(header, button);
+let count: number = 0;
+const countDisplay: HTMLDivElement = document.createElement("div");
+countDisplay.innerText = `You have eaten ${count} pizzas!`;
+
+button.addEventListener("click", () => {
+  count++;
+  countDisplay.innerText = `You have eaten ${count} pizzas!`;
+});
+
+app.append(header, button, countDisplay);
