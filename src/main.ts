54456bad64b3b963656850a1b0bf09870241c338
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-10-09 14:37:52 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+/* Increase count by 1 and update display with correct count*/
+function updateCount() {
+  count++;
+  countDisplay.innerText = `You have eaten ${count} pizzas!`;
+}
 
+const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Pizza Party";
 document.title = gameName;
 
+// Game Title
 const header: HTMLHeadElement = document.createElement("h1");
 header.innerHTML = gameName;
 
+// Button
 const button: HTMLButtonElement = document.createElement("button");
 button.textContent = "🍕";
 
 let count: number = 0;
+
+// Count Display
 const countDisplay: HTMLDivElement = document.createElement("div");
 countDisplay.innerText = `You have eaten ${count} pizzas!`;
 
-button.addEventListener("click", () => {
-  count++;
-  countDisplay.innerText = `You have eaten ${count} pizzas!`;
-});
+// Increase count every sec + whenever button is clicked
+button.addEventListener("click", updateCount);
+setInterval(updateCount, 1000);
 
+// Add elements to app
 app.append(header, button, countDisplay);
